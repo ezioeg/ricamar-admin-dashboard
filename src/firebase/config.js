f5ffcc3b0e6b664fb9335e0477f7ebb4d837ed77
akmalkhan1 <akmall.khan1@gmail.com>
2021-05-20 14:52:24 -0400

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0_nPilu_un7oqk278mBjh4gvWNLsKyCo",
+  authDomain: "ricamar-ca.firebaseapp.com",
+  projectId: "ricamar-ca",
+  storageBucket: "ricamar-ca.appspot.com",
+  messagingSenderId: "148480224436",
+  appId: "1:148480224436:web:3f6d40c097324d3d3c3be1",
+  measurementId: "G-DMLWE1X33G",
+};
+
+export default firebaseConfig;
